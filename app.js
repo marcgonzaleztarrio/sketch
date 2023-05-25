ba0d7eecf9144cea7478649e76f773f59f274fa0
TumarreTorre <marcgonzaleztarrio@gmail.com>
2023-05-25 12:42:50 +0200

    nice

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
 const DEFAULT_COLOR = "#333333";
+const DEFAULT_SIZE = 16;
+const DEFAULT_BACKGROUND_COLOR = "#ff0000";
 
 let currentColor = DEFAULT_COLOR;
 
 const container = document.getElementById("container");
 let sizeInput = document.getElementById("sizeGrid");
-
-size = 16;
+let textoSize = document.getElementById("textoSizeGrid");
+let colorInput = document.getElementById("color");
+let textoColorGrid = document.getElementById("textoColorGrid");
+let colorBG = document.getElementById("colorBG");
+let textoColorBG = document.getElementById("textoColorBG");
 
 function gridElement(size) {
-  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  setTimeout(() => {
+    size = size || DEFAULT_SIZE;
 
-  console.log("loko2 function ");
+    textoColorGrid.innerHTML = colorInput.value;
+    textoColorBG.innerHTML = colorBG.value;
 
-  for (let i = 0; i < size * size; i++) {
-    var div = document.createElement("div");
-    div.classList.add("grid-item");
-    div.innerHTML = "";
-    div.addEventListener("mousedown", changeColor);
-    div.addEventListener("mouseover", changeColor);
-    container.appendChild(div);
-  }
+    textoSize.innerHTML = `${sizeInput.value} X ${sizeInput.value}`;
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+      var div = document.createElement("div");
+      div.classList.add("grid-item");
+      div.style.removeProperty("background-color");
+      div.innerHTML = "";
+      container.appendChild(div);
+
+      div.addEventListener("mousedown", changeColorGrid);
+      div.addEventListener("mouseenter", changeColorGrid);
+    }
+
+    colorBG.addEventListener("change", (e) => {
+      console.log("suuuuuususuususuu");
+      container.querySelectorAll(".grid-item").forEach((el) => {
+        el.style.background = colorBG.value;
+        textoColorBG.innerHTML = colorBG.value;
+      });
+    });
+
+    sizeInput.addEventListener("change", (e) => {
+      container.querySelectorAll(".grid-item").forEach((el) => el.remove());
+      gridElement(sizeInput.value);
+    });
+  }, 120);
 }
 
-function changeColor(e) {
+console.log(container.style);
+
+function changeColorGrid(e) {
+  colorInput.addEventListener("change", (e) => {
+    currentColor = colorInput.value;
+    textoColorGrid.innerHTML = colorInput.value;
+  });
+
   return (e.target.style.backgroundColor = currentColor);
 }
-
-sizeInput.addEventListener("change", (e) => {
-  gridElement;
-});
 
 document.body.onload = gridElement();
